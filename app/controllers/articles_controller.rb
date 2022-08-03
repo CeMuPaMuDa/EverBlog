d9496362153d9e32412936d4b0584fd7ad4f1b9d
CeMuPaMuDa <bepyc9i@gmail.com>
2022-08-03 22:40:26 +0300

    fix permit parametrs

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,19 +1,16 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: %i[ show update destroy ]
 
-  # GET /articles
   def index
     @articles = Article.all
 
     render json: @articles
   end
 
-  # GET /articles/1
   def show
     render json: @article
   end
 
-  # POST /articles
   def create
     @article = Article.new(article_params)
 
@@ -24,7 +21,6 @@ class ArticlesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /articles/1
   def update
     if @article.update(article_params)
       render json: @article
@@ -33,19 +29,17 @@ class ArticlesController < ApplicationController
     end
   end
 
-  # DELETE /articles/1
+
   def destroy
     @article.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_article
       @article = Article.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def article_params
-      params.fetch(:article, {})
+      params.require(:article).permit(:title, :body)
     end
 end
