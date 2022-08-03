700557a474eef5888b085d7c8568e14a41b048cc
CeMuPaMuDa <bepyc9i@gmail.com>
2022-08-03 19:25:48 +0300

    add article controller
    remove article controller and add scaffold controller (react page worked)

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://http://localhost:3005/"
+    origins "http://localhost:3005"
 
     resource "*",
       headers: :any,
