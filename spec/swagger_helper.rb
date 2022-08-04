a32968b97079aa3d3344ba5a7a2f44a04ece60cd
CeMuPaMuDa <bepyc9i@gmail.com>
2022-08-04 18:42:56 +0300

    fix url

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -24,7 +24,7 @@ RSpec.configure do |config|
       paths: {},
       servers: [
         {
-          url: '{defaultHost}',
+          url: 'http://{defaultHost}',
           variables: {
             defaultHost: {
               default: '127.0.0.1:3000/'
