5cbb6d13dde5c116d44f7c1cc72b3c228fd2dd7e
CeMuPaMuDa <bepyc9i@gmail.com>
2022-08-04 14:42:22 +0300

    added comments to migration

--- db/migrate/20220803154615_create_articles.rb
+++ db/migrate/20220803154615_create_articles.rb
@@ -2,10 +2,11 @@
 
 class CreateArticles < ActiveRecord::Migration[7.0]
   def change
-    create_table :articles do |t|
-      t.string :title
-      t.text :body
-
+    create_table  :articles, comment: 'article table' do |t|
+      t.string    :title,
+                  comment: 'article header'
+      t.text      :body,
+                  comment: 'article content'
       t.timestamps
     end
   end
